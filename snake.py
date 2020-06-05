bf4f8a66f22c101dfba674c8a981e12c2099d24b
AugmenTab <tbaum92@live.com>
2020-06-04 20:06:47 -0600

    Set up function to draw the grid.

--- snake.py
+++ snake.py
@@ -31,11 +31,20 @@ class snake(object):
         pass
 
 def drawGrid(w, rows, surface):
-    pass
+    sizeBtwn = w // rows
+    x = 0
+    y = 0
+    for l in range(rows):
+        x = x + sizeBtwn
+        y = y + sizeBtwn
+
+        pygame.draw.line(surface, (255,255,255), (x,0), (x,w))
+        pygame.draw.line(surface, (255,255,255), (0,y), (w,y))
 
 def redrawWindow(surface):
+    global rows, width
     win.fill((0,0,0))
-    drawGrid(surface)
+    drawGrid(width, rows, surface)
     pygame.display.update()
 
 def randomSnack(rows, items):
@@ -45,10 +54,10 @@ def message_box(subject, content):
     pass
 
 def main():
+    global width, rows
     width = 500
-    height = 500
     rows = 20
-    win = pygame.display.set_mode((width, height))
+    win = pygame.display.set_mode((width, width))
     s = snake((255,0,0), (10,10))
     flag = True
 
