a856cd26e1ba209983f848b4b4099dbf6f627182
AugmenTab <tbaum92@live.com>
2020-06-04 20:01:07 -0600

    Set up window and main section.

--- snake.py
+++ snake.py
@@ -43,13 +43,20 @@ def message_box(subject, content):
     pass
 
 def main():
+    width = 500
+    height = 500
+    rows = 20
+    win = pygame.display.set_mode((width, height))
+    s = snake((255,0,0), (10,10))
+    flag = True
+
+    clock = pygame.time.Clock()
+
+    while flag:
+        pygame.time.delay(50)
+        clock.tick(10)
+
+        redrawWindow(win)
     pass
-
-rows = 
-w = 
-h = 
-
-cube.rows = rows
-cube.w = wrapper_descriptor
 
 main()
